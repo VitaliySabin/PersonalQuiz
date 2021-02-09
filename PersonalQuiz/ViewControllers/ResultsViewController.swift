f5ffe79dd85221ccac95486cf1e265a8fdcba212
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2021-02-09 20:58:11 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 08.02.2021.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    // Передать сюда массив с ответами
+    // Определить наиболее часто встречающийся тип животного
+    // Отобразить результат в соответсвии с этим животным.
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
